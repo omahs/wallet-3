09a24ccad1668e53416ee28b2bba6b5df0004798
MuckT <mcgtom10@gmail.com>
2023-12-02 01:21:08 -0800

    feat: sms invites from contacts in send flow

--- src/components/InviteOptionsModal.tsx
+++ src/components/InviteOptionsModal.tsx
@@ -1,21 +1,36 @@
 import getPhoneHash from '@celo/phone-utils/lib/getPhoneHash'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Share } from 'react-native'
+import Share, { ShareSingleOptions, Social } from 'react-native-share'
 import { useSelector } from 'react-redux'
 import { InviteEvents } from 'src/analytics/Events'
 import ValoraAnalytics from 'src/analytics/ValoraAnalytics'
 import { INVITE_REWARDS_NFTS_LEARN_MORE, INVITE_REWARDS_STABLETOKEN_LEARN_MORE } from 'src/config'
 import { inviteModal } from 'src/images/Images'
-import { useShareUrl } from 'src/invite/hooks'
 import InviteModal from 'src/invite/InviteModal'
-import { getDisplayName, Recipient } from 'src/recipients/recipient'
+import { useShareUrl } from 'src/invite/hooks'
+import { Recipient, getDisplayName } from 'src/recipients/recipient'
 import { inviteRewardsActiveSelector, inviteRewardsTypeSelector } from 'src/send/selectors'
 import { InviteRewardsType } from 'src/send/types'
+import Logger from 'src/utils/Logger'
 
+const TAG = 'invite/InviteOptionsModal'
 interface Props {
   recipient: Recipient
   onClose(): void
+}
+
+export const shareSMS = async (phoneNumber = '', message: string) => {
+  try {
+    const shareOptions: ShareSingleOptions = {
+      message: message,
+      recipient: phoneNumber.substring(1),
+      social: Social.Sms,
+    }
+    return await Share.shareSingle(shareOptions)
+  } catch (error) {
+    Logger.error(TAG, 'shareSMS', error)
+  }
 }
 
 const InviteOptionsModal = ({ recipient, onClose }: Props) => {
@@ -23,10 +38,11 @@ const InviteOptionsModal = ({ recipient, onClose }: Props) => {
   const link = useShareUrl()
   const inviteRewardsActive = useSelector(inviteRewardsActiveSelector)
   const inviteRewardsType = useSelector(inviteRewardsTypeSelector)
+  const smsAvailable = Share.isPackageInstalled(Social.Sms)
 
-  const handleShareInvite = async () => {
-    if (link) {
-      await Share.share({ message })
+  const handleSmsShareInvite = async () => {
+    if (link && (await smsAvailable)) {
+      await shareSMS(recipient?.e164PhoneNumber, message)
       ValoraAnalytics.track(InviteEvents.invite_with_share, {
         phoneNumberHash: recipient.e164PhoneNumber ? getPhoneHash(recipient.e164PhoneNumber) : null,
       })
@@ -72,7 +88,7 @@ const InviteOptionsModal = ({ recipient, onClose }: Props) => {
       disabled={!link}
       helpLink={helpLink}
       onClose={handleClose}
-      onShareInvite={handleShareInvite}
+      onShareInvite={handleSmsShareInvite}
     />
   )
 }
